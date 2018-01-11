30d6eba7bd6051fdaa57227bff1365247f242817
Martin Brychta <martin@brychta.name>
2018-01-12 00:05:36 +0100

    Update all tools to check if containers are running, run then if needed

--- interpreters/windows/importdb.bat
+++ interpreters/windows/importdb.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 
+FOR /f %%i IN ('docker ps -q -f name^=database') DO SET IS_RUNNING=%%i
+IF /I [%IS_RUNNING%]==[] (CALL dcup)
+
 SET DB=""
 SET FILE=%1
 
--- interpreters/windows/migratedb.bat
+++ interpreters/windows/migratedb.bat
@@ -1,3 +1,6 @@
 @echo off
 
+FOR /f %%i IN ('docker ps -q -f name^=web') DO SET IS_RUNNING=%%i
+IF /I [%IS_RUNNING%]==[] (CALL dcup)
+
 docker exec web php index.php migrations:migrate
