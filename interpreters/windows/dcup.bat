7a333203d92726b0a0ef64bfa1c7df1f3a2ad5ae
Martin Brychta <martin@brychta.name>
2018-01-12 20:51:42 +0100

    Update dcup command with extra checks for used containers

--- interpreters/windows/dcup.bat
+++ interpreters/windows/dcup.bat
@@ -1,14 +1,36 @@
 @echo off
 
 FOR /f %%i IN ('docker ps -q -f name^=web') DO SET IS_RUNNING=%%i
-FOR /f %%j IN ('docker ps -aq -f status^=exited -f name^=web') DO SET IS_EXITED=%%j
+IF /I [%IS_RUNNING%]==[] (GOTO STOPPED) ELSE (GOTO RUNNING)
 
-IF /I [%IS_RUNNING%]==[] (
-    IF /I NOT [%IS_EXITED%]==[] (
-        echo Removing old containers
-        docker rm web database phpmyadmin
-    )
-    docker-compose up -d --no-color
+:RUNNING
+FOR /f "delims=" %%i IN ('docker inspect -f "{{range .Mounts}}{{if eq .Destination \"/var/www\"}}{{.Source}}{{end}}{{end}}" web') DO SET CONTAINER_PATH=%%i
+FOR %%i IN (%CONTAINER_PATH:/= %) DO SET CONTAINER_PATH=%%i
+FOR %%i IN (%cd:\= %) DO SET CURRENT_PATH=%%i
+IF /I [%CONTAINER_PATH%]==[%CURRENT_PATH%] (GOTO EXEC) ELSE (
+    ECHO Warning! Current containers were created from different directory.
+    GOTO SHOULDRESTART
 )
 
+:STOPPED
+FOR /f %%i IN ('docker ps -aq -f status^=exited -f name^=web') DO SET IS_EXITED=%%i
+IF /I NOT [%IS_EXITED%]==[] (
+    ECHO Removing old containers
+    docker rm web database phpmyadmin
+)
+GOTO START
+
+:SHOULDRESTART
+IF EXIST docker-compose.yml (
+    ECHO Found docker-compose.yml in current directory. Will destroy old containers and create new ones.
+    docker stop web database phpmyadmin
+    GOTO STOPPED
+) ELSE (GOTO EXEC)
+
+:START
+docker-compose up -d --no-color
+GOTO EXEC
+
+
+:EXEC
 IF /I "%~1"=="-i" (docker exec -it web bash)
