be85e550e548585b93ad27ad161d570cc90fa126
Martin Brychta <martin@brychta.name>
2018-11-06 15:06:24 +0100

    Add chrome Docker image for remote puppeteer

---
+++ docker/chrome/etc/startup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+nohup chromium-browser \
+	--disable-background-networking \
+	--disable-default-apps \
+	--disable-extensions \
+	--disable-gpu \
+	--disable-sync \
+	--disable-translate \
+	--headless \
+	--hide-scrollbars \
+	--ignore-certificate-errors \
+	--ignore-certificate-errors-spki-list \
+	--ignore-ssl-errors \
+	--metrics-recording-only \
+	--mute-audio \
+	--no-first-run \
+	--no-sandbox \
+	--remote-debugging-port=9222 \
+	--remote-debugging-address=0.0.0.0 \
+	--safebrowsing-disable-auto-update \
+	--user-data-dir=/tmp
