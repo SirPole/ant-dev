b774daaf4d74af0a4376b306f0b5e8f1b669e9ab
Martin Brychta <martin@brychta.name>
2018-11-15 22:39:00 +0100

    Fix ant-down on windows

--- interpreters/windows/ant-down.bat
+++ interpreters/windows/ant-down.bat
@@ -1,8 +1,9 @@
 @echo off
 
-IF EXIST %userprofile%/.ant/.ant-dev-lock (
-    FOR /f "delims=" %%i IN ('more %userprofile%/.ant/.ant-dev-lock') DO SET LOCK_DIR=%%i
-    cd %LOCK_DIR%
-    docker-compose down
-    DEL /F /Q %userprofile%/.ant/.ant-dev-lock
-)
+IF EXIST %userprofile%\.ant\.ant-dev-lock (GOTO DOWN) ELSE (EXIT)
+
+:DOWN
+FOR /f "delims=" %%i IN ('more %userprofile%\.ant\.ant-dev-lock') DO SET LOCK_DIR=%%i
+cd %LOCK_DIR%
+docker-compose down
+DEL /F /Q %userprofile%\.ant\.ant-dev-lock
