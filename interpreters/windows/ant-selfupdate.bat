ce5b127355468f54c300e8dab4145c3defed9101
Martin Brychta <martin@brychta.name>
2018-11-15 22:15:21 +0100

    Update ant-selfupdate tool

--- interpreters/windows/ant-selfupdate.bat
+++ interpreters/windows/ant-selfupdate.bat
@@ -5,15 +5,26 @@ IF errorlevel 1 (SCHTASKS /Create /tn "Update ant-dev" /tr "PowerShell -WindowSt
 
 CD /D %~dp0/../..
 
+FOR /f %%i IN ('git rev-parse --abbrev-ref HEAD') DO SET BRANCH=%%i
+
 git fetch --all
-::git stash
+git stash save
 git checkout master --force
 git reset --hard origin/master
-::git stash apply
+git checkout %branch% --force
+git stash pop
 
-docker pull -a sirpole/ant-dev
+docker pull sirpole/ant-dev:chrome
+docker pull sirpole/ant-dev:httpd
+docker pull sirpole/ant-dev:php-7.2
+docker pull sirpole/ant-dev:php-7.1
+docker pull sirpole/ant-dev:php-5.6
 docker pull mysql
 docker pull phpmyadmin/phpmyadmin
+docker pull redis:alpine
+docker pull erikdubbelboer/phpredisadmin
+docker pull node:8-alpine
+docker pull composer
 docker system prune -f
 
 powershell -Command "[int][double]::Parse((Get-Date -UFormat %s))" > %userprofile%/.ant/.lastupdate
