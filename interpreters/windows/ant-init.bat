8350ae0dde00376efc0a2d8542b21cfa67cd69e5
Martin Brychta <martin@brychta.name>
2018-11-16 10:14:52 +0100

    Add ant init tool

---
+++ interpreters/windows/ant-init.bat
@@ -0,0 +1,7 @@
+@echo off
+
+COPY %~dp0..\..\dist\docker-compose.yml %cd%
+
+IF /I NOT "%~1"=="" (
+    powershell -Command "(Get-content %cd%\docker-compose.yml) | Foreach-Object {$_ -replace '7.2', '%~1'} | Set-Content %cd%\docker-compose.yml"
+)
