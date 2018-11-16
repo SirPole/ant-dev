eb80bc4c627d88e03ee741de7b0599b381a5f376
Martin Brychta <martin@brychta.name>
2018-11-16 22:05:55 +0100

    Kill possible orphans during ant down

--- interpreters/windows/ant-down.bat
+++ interpreters/windows/ant-down.bat
@@ -5,5 +5,5 @@ IF EXIST %userprofile%\.ant\.ant-dev-lock (GOTO DOWN) ELSE (EXIT)
 :DOWN
 FOR /f "delims=" %%i IN ('more %userprofile%\.ant\.ant-dev-lock') DO SET LOCK_DIR=%%i
 cd %LOCK_DIR%
-docker-compose down
+docker-compose down --remove-orphans
 DEL /F /Q %userprofile%\.ant\.ant-dev-lock
