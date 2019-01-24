e253d24e209a4bb6d9cc108389e9d6290cf59dfb
Martin Brychta <martin@brychta.name>
2019-01-25 00:05:28 +0100

    Add ant push command
    For development purposes only

---
+++ interpreters/windows/ant-push.bat
@@ -0,0 +1,10 @@
+@echo off
+
+docker push sirpole/ant-dev:chrome
+docker push sirpole/ant-dev:httpd
+docker push sirpole/ant-dev:node
+docker push sirpole/ant-dev:php-7.3
+docker push sirpole/ant-dev:php-7.2
+docker push sirpole/ant-dev:php-7.1
+docker push sirpole/ant-dev:php-5.6
+docker push sirpole/ant-dev:docker
