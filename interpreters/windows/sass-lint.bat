59053090694f39a172edfd72d4906fceeeef7a29
Martin Brychta <martin@brychta.name>
2019-02-04 14:51:15 +0100

    Reuse created node container

--- interpreters/windows/sass-lint.bat
+++ interpreters/windows/sass-lint.bat
@@ -1,8 +1,19 @@
 @echo off
 
+FOR /f %%i IN ('docker ps -q -f name^=node') DO SET IS_RUNNING=%%i
+IF /I [%IS_RUNNING%]==[] (GOTO START) ELSE (GOTO CONNECT)
+
+:START
 docker run ^
---rm ^
---network ant-dev ^
---volume %cd%:/app ^
---volume %userprofile%\.ant\npm:/tmp ^
-sirpole/ant-dev:node sass-lint -v %*
+    --rm ^
+    --name node ^
+    --network ant-dev ^
+    --publish 3000:3000 ^
+    --volume %cd%:/app ^
+    --volume %userprofile%\.ant\npm:/tmp ^
+    sirpole/ant-dev:node sass-lint %*
+GOTO :EOF
+
+:CONNECT
+docker exec ^
+    node sass-lint %*
