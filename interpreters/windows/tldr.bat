bc8855c5dbae3e6e0bb3c7c4e81677491315493d
Martin Brychta <martin@brychta.name>
2019-02-04 14:51:15 +0100

    Reuse created node container

--- interpreters/windows/tldr.bat
+++ interpreters/windows/tldr.bat
@@ -1,10 +1,23 @@
 @echo off
 
+FOR /f %%i IN ('docker ps -q -f name^=node') DO SET IS_RUNNING=%%i
+IF /I [%IS_RUNNING%]==[] (GOTO START) ELSE (GOTO CONNECT)
+
+:START
 winpty docker run ^
---rm ^
---interactive ^
---tty ^
---network ant-dev ^
---volume %cd%:/app ^
---volume %userprofile%\.ant\npm:/tmp ^
-sirpole/ant-dev:node tldr %*
+    --rm ^
+    --interactive ^
+    --tty ^
+    --name node ^
+    --network ant-dev ^
+    --publish 3000:3000 ^
+    --volume %cd%:/app ^
+    --volume %userprofile%\.ant\npm:/tmp ^
+    sirpole/ant-dev:node tldr %*
+GOTO :EOF
+
+:CONNECT
+winpty docker exec ^
+    --interactive ^
+    --tty ^
+    node tldr %*
