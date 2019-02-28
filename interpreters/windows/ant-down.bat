e9eb0effdcf9df5ee57e01d4abc13d291cda5184
Martin Brychta <martin@brychta.name>
2019-02-28 16:28:38 +0100

    Use nfs for volumes

--- interpreters/windows/ant-down.bat
+++ interpreters/windows/ant-down.bat
@@ -5,5 +5,8 @@ IF EXIST %userprofile%\.ant\.ant-dev-lock (GOTO DOWN) ELSE (EXIT)
 :DOWN
 FOR /f "delims=" %%i IN ('more %userprofile%\.ant\.ant-dev-lock') DO SET LOCK_DIR=%%i
 cd %LOCK_DIR%
-docker-compose down --remove-orphans
-DEL /F /Q %userprofile%\.ant\.ant-dev-lock
+docker-compose down ^
+    --remove-orphans ^
+    --volumes
+taskkill /f /im "winnfsd.exe" >nul
+DEL /F /Q %userprofile%\.ant\.ant-dev-lock %userprofile%\.ant\.ant-dev-nfs-exports
